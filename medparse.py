10268cb4abf4bd54e98e93a7bdeb8560cf7cdfa7
Tatiana Ivanova <tanushaivanova97@gmail.com>
2020-06-21 15:08:27 +0300

    Rewrote code from functional to class

--- medparse.py
+++ medparse.py
@@ -1,59 +1,80 @@
 import requests
+from requests.adapters import HTTPAdapter
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from lxml import etree
-from constants import URL_SEARCH
+from urllib3.util.retry import Retry
+from constants import URL_SEARCH, XPATH_URLS
 
 
-def get_data_from_table(response, data):
-    items = list()
-    soup = BeautifulSoup(response, 'lxml')
-    divs = soup.select('div.trow')
-    xpath_urls = {
-        'name': '//div[contains(@class, "name")]//text()',
-        'country': '//div[contains(@class, "country")]//text()',
-        'name_pharm': '//div[contains(@class, "pharm")]//text()',
-        'address': '//div[contains(@class, "address")]/a/text()',
-        'address_coord': '//div[contains(@class, "address")]/a/@href',
-        'phone': '//div[contains(@class, "address")]/text()',
-        'date': '//div[contains(@class, "date")]//text()',
-        'pricefull': '//div[contains(@class, "pricefull")]//text()'
-    }
-    for div in divs:
-        div = str(div)
-        tree = etree.XML(div)
-        res = dict()
-        for key, value in xpath_urls.items():
-            item = tree.xpath(value)
-            if item:
-                if len(item) > 1:
-                    item = ''.join(item).replace(',', '').strip()
-                    item = [item]
-                res[key] = item[0]
-        if res.keys():
-            items.append(res)
-    data['results'] = items
-    return data
+class MedParse:
+    __url_search = URL_SEARCH
+    _xpath_urls = XPATH_URLS
 
+    def __init__(self, region, area, search_str):
+        self.__region = region
+        self.__area = area
+        self._search_str = search_str
 
-def get_inform(url: str, search_str: str, sreg: int, dist: int):
-    # search_str - search string
-    # sreg - city
-    # dist - district
-    data = {
-        'free_str': search_str,
-        'sreg': sreg,
-        'dist': dist
-    }
-    response = requests.post(url, data, allow_redirects=True).content.decode('utf-8')
-    soup = BeautifulSoup(response, 'lxml')
-    options = soup.select('option')
-    if options:
-        href = options[1]['value']
-        url_next = urljoin(url, href)
-        response = requests.get(url_next).content.decode('utf-8')
-    results = get_data_from_table(response, data)
-    return results
+    def __str__(self):
+        return 'Class for parse med'
+
+    def get_inform(self):
+        # search_str - search string
+        # sreg - city (region)
+        # dist - district ()
+        data = {
+            'free_str': self._search_str,
+            'sreg': self.__region,
+            'dist': self.__area
+        }
+        response = self.request_with_retry('POST', self.__url_search, data)
+        # requests.post(self.__url_search, data, allow_redirects=True, max_retries=3, dely_between_retries=3).content.decode('utf-8')
+        soup = BeautifulSoup(response, 'lxml')
+        options = soup.select('option')
+        if options:
+            href = options[1]['value']
+            url_next = urljoin(self.__url_search, href)
+            response = self.request_with_retry('GET', request_url=url_next)
+            # response = requests.get(url_next).content.decode('utf-8')
+        results = self.get_data_from_table(response, data)
+        return results
+
+    def get_data_from_table(self, response, data):
+        items = list()
+        soup = BeautifulSoup(response, 'lxml')
+        divs = soup.select('div.trow')
+        for div in divs:
+            div = str(div)
+            tree = etree.XML(div)
+            res = dict()
+            for key, value in self._xpath_urls.items():
+                item = tree.xpath(value)
+                if item:
+                    if len(item) > 1:
+                        item = ''.join(item).replace(',', '').strip()
+                        item = [item]
+                    res[key] = item[0]
+            if res.keys():
+                items.append(res)
+        data['results'] = items
+        return data
+
+    def request_with_retry(self, method, request_url, data=None):
+        session = requests.Session()
+
+        retries = Retry(
+            total=5,
+            backoff_factor=0.7,
+            status_forcelist=[500, 502, 503, 504]
+
+        )
+        session.mount('https://', HTTPAdapter(max_retries=5))
+
+        if method == 'GET':
+            return requests.get(url=request_url, timeout=1, allow_redirects=True).content.decode('utf-8')
+        elif method == 'POST':
+            return requests.post(url=request_url, timeout=1, allow_redirects=True, data=data).content.decode('utf-8')
 
 
 if __name__ == '__main__':
@@ -61,5 +82,6 @@ if __name__ == '__main__':
     sreg = 78
     dist = 9
     search_str = 'парацетамол'
-    results = get_inform(url=url, search_str=search_str, sreg=sreg, dist=dist)
-    print(results)
+    med_parse = MedParse(sreg, dist, search_str)
+    print(med_parse)
+    print(med_parse.get_inform())
